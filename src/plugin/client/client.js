c5e05e01ffbdc025ea5bda009390463369e598f7
Patrik Stutz <patrik.stutz@gmail.com>
2016-11-26 21:18:49 +0100

    moved client to async/await

--- src/plugin/client/client.js
+++ src/plugin/client/client.js
@@ -1,43 +1,23 @@
 var client = require("require")("boxify/lib/client.js");
 var cookie = require("cookies-js");
 
+class Client{
+    async createPin(data){
+        return await client.getText("/api/pins/create",{method:"POST",jsonBody:data});
+    }
+    async getPins(){
+        return await client.getJson("/api/pins",{method:"POST",jsonBody:{}});
+    }
+    async updatePin(id,data){
+        return await client.execute("/api/pins/"+id+"/update",{method:"POST",jsonBody:data});
+    }
+    async deletePin(id){
+        return await client.execute("/api/pins/"+id+"/delete",{method:"POST"});
+    }
+    async login(pin,cb){
+        var session = await client.getJson("/api/pins/login",{method:"POST",jsonBody:{pin:pin}});
+        cookie.set("session",session._id,{expires:60*60*24*365*100});
+    }
+}
+
 module.exports = new Client();
-
-function Client(){
-
-}
-Client.prototype.createPin = function(data,cb){
-    client.getText("/api/pins/create",{method:"POST",jsonBody:data})
-        .then(function(id){
-            cb(null,id);
-        }).catch(cb);
-}
-Client.prototype.getPins = function(cb){
-    client.getJson("/api/pins",{method:"POST",jsonBody:{}})
-        .then(function(pins){
-            cb(null,pins);
-        }).catch(cb);
-}
-Client.prototype.updatePin = function(id,data,cb){
-    client.execute("/api/pins/"+id+"/update",{method:"POST",jsonBody:data})
-        .then(function(){
-            cb(null);
-        }).catch(cb);
-}
-Client.prototype.deletePin = function(id,cb){
-    client.execute("/api/pins/"+id+"/delete",{method:"POST"})
-        .then(function(){
-            cb(null);
-        }).catch(cb);
-}
-Client.prototype.login = function(pin,cb){
-    client.getJson("/api/pins/login",{method:"POST",jsonBody:{pin:pin}})
-        .then(function(session){
-            cookie.set("session",session._id,{expires:60*60*24*365*100});
-            client.getSession().then(function(sess){
-                cb(null,sess);
-            }).catch(cb);
-        })
-        .catch(cb)
-
-}
