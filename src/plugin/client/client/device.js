fdf7c6ab058272c7cba6a7e618fccf9b04dbb9cd
Patrik Stutz <patrik.stutz@gmail.com>
2016-11-27 00:32:59 +0100

    use class syntax for DeviceClient

--- src/plugin/client/client/device.js
+++ src/plugin/client/client/device.js
@@ -1,26 +1,32 @@
 var devices = require("../../../devices");
 
-var Device = module.exports = function Device(name){
-    this.name = name;
-    this.state = {};
-    var commands = devices[name].commands;
-    for(var i = 0; i < commands.length; i++){
-        (function(command){
-            this[command] = function(){
-                var req = new XMLHttpRequest();
-                req.open("POST","/api/electronic/"+name+"/"+command)
-                req.setRequestHeader("Content-Type","application/json");
-                req.onreadystatechange = function(){
-                    if(req.readyState == 4){
-                        //done
-                    }
+class Device{
+    constructor(name){
+        this.name = name;
+        this.state = {};
+        var commands = devices[name].commands;
+        for(var i = 0; i < commands.length; i++){
+            ((command)=>{
+                this[command] = async function(){
+                    var args = Array.prototype.slice.call(arguments);
+                    await new Promise((s)=>{
+                        var req = new XMLHttpRequest();
+                        req.open("POST","/api/electronic/"+name+"/"+command)
+                        req.setRequestHeader("Content-Type","application/json");
+                        req.onreadystatechange = function(){
+                            if(req.readyState == 4){
+                                s();
+                            }
+                        }
+                        req.send(JSON.stringify());
+                    })
                 }
-                req.send(JSON.stringify(Array.prototype.slice.call(arguments)));
-            }
-        }).bind(this)(commands[i])
+            })(commands[i])
+        }
+    }
+    updateState(state){
+        this.state = state;
     }
 }
 
-Device.prototype.updateState = function(state){
-    this.state = state;
-}
+module.exports = Device;
